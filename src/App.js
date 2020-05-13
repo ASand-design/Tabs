08df716d4235c78f3c5de7a44e69578f654ceb48
ASand-design <59930866+ASand-design@users.noreply.github.com>
2020-05-13 08:50:38 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import Tabs from './components/Tabs';
+
+const items = [
+  {
+    label: 'Tab 1',
+    content: 'Tab 1 content'
+  },
+  {
+    label: 'Tab 2',
+    content: 'Tab 2 content'
+  },
+  {
+    label: 'Tab 3',
+    content: 'Tab 3 content'
+  },
+]
 
 function App() {
   return (
     <div className="App">
-    
+      <Tabs items={items}/>
     </div>
   );
 }
--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,1 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './Tabs.module.css';
+
+function Tabs( {items} ) {
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    return (
+        <div>
+            <div className={styles.headers}>
+                {items.map((item, i) => {
+                    return (
+                        <div
+                         key={i}
+                         className={styles.header + (i == activeIndex ?
+                         ' ' + styles.active : '')}
+                         onClick={() => setActiveIndex(i)}
+                         >
+                        {item.label}</div>
+                    )
+                })}
+            </div>
+            <div className={styles.content}>{props.items[activeIndex]
+            .content}</div>
+        </div>
+    )
+}
+export default Tabs;
